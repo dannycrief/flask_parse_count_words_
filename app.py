53b0ec6f64da3e8c58e2f773928c4d2b5b2c0519
Stepan Kozurak <step.kozbvb@gmail.com>
2020-07-28 17:04:36 +0200

    Improved app on local machine. Updated dockerfile and docker-compose

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import os
 import requests
 import enum
 from sqlalchemy import Enum
@@ -7,20 +8,18 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request
 import logging
 
-logging.basicConfig(filename="../app.log", level=logging.INFO, format="%(levelname)s: %(message)s")
+logging.basicConfig(filename="app.log", level=logging.INFO, format="%(levelname)s: %(message)s")
+
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=os.environ.get('POSTGRES_USER', 'postgres'),
+                                                               pw=os.environ.get('POSTGRES_PW', '211217ns'),
+                                                               url=os.environ.get('POSTGRES_URL', '127.0.0.1:5432'),
+                                                               db=os.environ.get('POSTGRES_DB', 'parsing_site'))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-celery = Celery(app.name, broker='redis://localhost', backend='redis://localhost')
-
-
-class Results(db.Model):
-    _id = db.Column(db.Integer, primary_key=True)
-    address = db.Column(db.String(300), unique=False, nullable=True)
-    words_count = db.Column(db.Integer, unique=False, nullable=True)
-    http_status_code = db.Column(db.Integer)
+celery = Celery(app.name, broker='redis://localhost', backend='db+' + DB_URL)
 
 
 class TaskStatus(enum.Enum):
@@ -30,17 +29,29 @@ class TaskStatus(enum.Enum):
 
 
 class Tasks(db.Model):
-    _id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key=True)
     address = db.Column(db.String(300), unique=False, nullable=True)
     timestamp = db.Column(db.DateTime())
     task_status = db.Column(Enum(TaskStatus))
     http_status = db.Column(db.Integer)
+    results = db.relationship('Results', uselist=False, back_populates="tasks")
 
 
-# TODO: check if only https or http user entered
+class Results(db.Model):
+    __tablename__ = 'results'
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(300), unique=False, nullable=True)
+    words_count = db.Column(db.Integer, unique=False, nullable=True)
+    http_status_code = db.Column(db.Integer)
+    task_id = db.Column(db.Integer, db.ForeignKey('tasks.id'))
+    tasks = db.relationship("Tasks", back_populates="results")
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
+        app.logger.info("Found GET method")
         return render_template('index.html')
     elif request.method == "POST":
         app.logger.debug("Found method POST")
@@ -53,7 +64,7 @@ def index():
             session = Tasks(address=received_link, timestamp=datetime.now(), task_status='NOT_STARTED')
             db.session.add(session)
             db.session.commit()
-            parsing_func.apply_async([session._id], countdown=3, expires=10)
+            parsing_func.apply_async([session.id], countdown=3, expires=10)
         return render_template('index.html')
 
 
@@ -71,16 +82,17 @@ def parsing_func(_id):
         if parse_url.ok:
             parse_url = parse_url.text.lower().split()
             words = parse_url.count('python')
-        result = Results(address=address, words_count=words,
-                         http_status_code=parse_status)
         task = Tasks.query.get(_id)
+        result = Results(address=address, words_count=words,
+                         http_status_code=parse_status, task_id=task.id)
+        task.http_status = int(parse_status)
         task.task_status = 'FINISHED'
         db.session.add(result)
         db.session.commit()
         app.logger.info("Task status: FINISHED and successfully committed")
 
 
-@app.route('/result')
+@app.route('/results')
 def show_results():
     results = Results.query.all()
     for i in results:
@@ -93,6 +105,21 @@ def show_results():
     return render_template('result.html', results=results)
 
 
-if __name__ == '__main__':
+@app.cli.command('resetdb')
+def resetdb_command():
+    from sqlalchemy_utils import database_exists, create_database, drop_database
+
+    if database_exists(DB_URL):
+        app.logger.warning("Deleting database")
+        drop_database(DB_URL)
+
+    if not database_exists(DB_URL):
+        app.logger.info("Creating database.")
+        create_database(DB_URL)
+
+    app.logger.info("Creating tables.")
     db.create_all()
+
+
+if __name__ == '__main__':
     app.run(debug=True)
